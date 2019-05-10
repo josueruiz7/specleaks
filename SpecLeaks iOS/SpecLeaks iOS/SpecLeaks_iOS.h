fbcf514db6c7fcf31000484d7458435ba2682ac0
Ruiz, Josué <josuruiz@paypal.com>
2019-05-10 13:08:19 -0600

    Add support to carthage

---
+++ SpecLeaks iOS/SpecLeaks iOS/SpecLeaks_iOS.h
@@ -0,0 +1,19 @@
+//
+//  SpecLeaks_iOS.h
+//  SpecLeaks iOS
+//
+//  Created by Ruiz, Josué on 5/10/19.
+//  Copyright © 2019 com.lmp.specleaks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpecLeaks_iOS.
+FOUNDATION_EXPORT double SpecLeaks_iOSVersionNumber;
+
+//! Project version string for SpecLeaks_iOS.
+FOUNDATION_EXPORT const unsigned char SpecLeaks_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpecLeaks_iOS/PublicHeader.h>
+
+
